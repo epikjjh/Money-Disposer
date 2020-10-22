a9c4d6a94d9e788d2de166c1fc8884282a73d471
2015004248 <epikjjh@gmail.com>
2020-10-22 18:55:40 +0900

    Create PUT & GET api & test 1. Create PUT api 2. Create GET api 3. Do test
    Signed-off-by: 2015004248 <epikjjh@gmail.com>

--- disposer/apiServer/views.py
+++ disposer/apiServer/views.py
@@ -5,7 +5,9 @@ from apiServer.models import Ticket, UserList
 import json
 import random
 import string
-from datetime import datetime
+from datetime import datetime, timedelta
+from pytz import timezone
+from functools import reduce
 
 
 class DisposeView(APIView):
@@ -21,6 +23,7 @@ class DisposeView(APIView):
             'HTTP_X_USER_ID'), request.META.get('HTTP_X_ROOM_ID')
         if not user_id or not room_id:
             return Response({"Error: missing required header"}, status=412)
+
         request = json.loads(request.body)
         num, amount = request['num'], request['amount']
         if not num or not amount:
@@ -38,7 +41,41 @@ class DisposeView(APIView):
             'HTTP_X_USER_ID'), request.META.get('HTTP_X_ROOM_ID')
         if not user_id or not room_id:
             return Response({"Error: missing required header"}, status=412)
-        return Response("PUT")
+
+        request = json.loads(request.body)
+        token = request['token']
+        if not token:
+            return Response({"Error: missing required data"}, status=412)
+
+        try:
+            ticket = Ticket.objects.get(token=token)
+        except Ticket.DoesNotExist:
+            return Response({"Error: unauthorized token"}, status=401)
+
+        if ticket.owner == user_id:
+            return Response({"Error: unauthorized user"}, status=401)
+
+        if datetime.now(timezone('Asia/Seoul')) - ticket.date > timedelta(minutes=10):
+            return Response({"Error: unauthorized time"}, status=401)
+
+        receivers = UserList.objects.filter(ticket=ticket)
+        receivers_num = receivers.count()
+        total_num = ticket.num
+        is_receive = receivers.filter(user=user_id).count()
+
+        if is_receive:
+            return Response({"Error: unauthorized user. Already received"}, status=401)
+
+        if receivers_num >= total_num:
+            return Response({"Error: unauthorized user. No remaining amount"}, status=401)
+
+        ret_amount = ticket.amount // total_num if receivers_num != total_num - 1 \
+            else ticket.amount // total_num + ticket.amount % total_num
+
+        new_list = UserList(ticket=ticket, user=user_id)
+        new_list.save()
+
+        return Response({"amount": ret_amount})
 
     # Retrieve api
     def get(self, request):
@@ -46,4 +83,28 @@ class DisposeView(APIView):
             'HTTP_X_USER_ID'), request.META.get('HTTP_X_ROOM_ID')
         if not user_id or not room_id:
             return Response({"Error: missing required header"}, status=412)
-        return Response("GET")
+
+        request = json.loads(request.body)
+        token = request['token']
+        if not token:
+            return Response({"Error: missing required data"}, status=412)
+
+        try:
+            ticket = Ticket.objects.get(token=token)
+        except Ticket.DoesNotExist:
+            return Response({"Error: unauthorized token"}, status=401)
+
+        if ticket.owner != user_id:
+            return Response({"Error: unauthorized user"}, status=401)
+
+        if datetime.now(timezone('Asia/Seoul')) - ticket.date > timedelta(days=7):
+            return Response({"Error: unauthorized time"}, status=401)
+
+        receivers = UserList.objects.filter(ticket=ticket)
+        receivers_num = receivers.count()
+        total_num = ticket.num
+        ret_info = [[ticket.amount // total_num if i+1 != total_num - 1
+                     else ticket.amount // total_num + ticket.amount % total_num, recevier.user] for i, recevier in enumerate(receivers)]
+        ret_amount = reduce(lambda x, y: x + y[0], ret_info, 0)
+
+        return Response({"date": ticket.date, "amount": ticket.amount, "received amount": ret_amount, "info": ret_info})
